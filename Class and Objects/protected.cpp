a11854653d7cd39c8c1e8cb7537fc4db31b1411c
Shridhar299 <105181932+Shridhar299@users.noreply.github.com>
2024-05-06 11:19:49 +0530

    Add files via upload

---
+++ Class and Objects/protected.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+// Base class
+class Parent {
+    // Protected data members
+protected:
+    int id_protected;
+};
+
+// Subclass or derived class
+class Child : public Parent {
+public:
+    void setId(int id) {
+        // Child class can access the inherited protected data members of the base class
+        id_protected = id;
+    }
+
+    void displayId() {
+        cout << "id_protected is: " << id_protected << endl;
+    }
+};
+
+// Main function
+int main() {
+    Child obj1;
+
+    // Member function of the derived class can access the protected data members of the base class
+    obj1.setId(81);
+    obj1.displayId();
+
+    return 0;
+}
+
